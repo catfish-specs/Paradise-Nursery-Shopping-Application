72aaffe8e0ab971cfb959cfc00bec407e4ed90ab
zgzg <pratitya.wankhade24@vit.edu>
2026-09-18 00:35:21 +0530

    Add AboutUs component for nursery information
    Introduced AboutUs component detailing the nursery's mission and offerings.

---
+++ AboutUs.jsx
@@ -0,0 +1,30 @@
+```jsx
+import React from "react";
+
+function AboutUs() {
+  return (
+    <div className="about-us">
+      <h1>About Paradise Nursery</h1>
+
+      <p>
+        Paradise Nursery is an online plant nursery that provides a variety
+        of beautiful and healthy houseplants for homes and offices.
+      </p>
+
+      <p>
+        Our goal is to make it easy for plant lovers to discover, choose,
+        and purchase plants that bring freshness and beauty to their spaces.
+      </p>
+
+      <h2>Our Mission</h2>
+
+      <p>
+        We aim to encourage people to bring more greenery into their everyday
+        lives by providing a simple and convenient plant shopping experience.
+      </p>
+    </div>
+  );
+}
+
+export default AboutUs;
+```
